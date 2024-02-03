e816e6e5075c206b913a308f3ba7609eb543404d
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-02-03 15:33:38 +0530

    5hr 35min
    setting offset

---
+++ Assets/scripts/worldgeneration/snowfloor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class snowfloor : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Material material;
+    public float offsetspeed = 0.2f;
+    private void Update()
+    {
+        transform.position = Vector3.forward * player.transform.position.z;
+        material.SetVector("Vector2_DAD7764F", new Vector2(0,-transform.position.z* offsetspeed));
+    }
+}
