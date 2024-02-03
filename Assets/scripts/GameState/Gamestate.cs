8f6916d119d2c54cbe31407baa2ce1a8f65c81e8
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-02-03 17:09:11 +0530

    5hr 40 adding game manager
    adding game manager

---
+++ Assets/scripts/GameState/Gamestate.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public abstract class Gamestate : MonoBehaviour
+{
+    protected GameManager brain;
+    private void Awake()
+    {
+        brain = GetComponent<GameManager>(); 
+    }
+    public virtual void Construct()
+    {
+         
+    }
+
+    public void Destruct()
+    {
+
+    }
+    public virtual void UpdateStatet()
+    {
+
+    }
+}
