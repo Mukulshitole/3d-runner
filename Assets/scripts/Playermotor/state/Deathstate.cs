7b0b28a31c422b88151d72113039ddcaece5a067
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-02-27 17:52:32 +0530

    play with the value of death script
    can make a game

--- Assets/scripts/Playermotor/state/Deathstate.cs
+++ Assets/scripts/Playermotor/state/Deathstate.cs
@@ -5,7 +5,7 @@ public class Deathstate : Basestate
     [SerializeField] private Vector3 knockbackForce = new Vector3(0, 4, -3);
     public override void Construct()
     {
-        //gamemanager.instance.changestate(gamemanager.instance.getcomponent<gamestatedeath>());
+        //Gamemanager.instance.changestate(Gamemanager.instance.getcomponent<gamestatedeath>());
     }
     //public override Vector3 ProcessMotion()
     //{
@@ -21,11 +21,14 @@ public class Deathstate : Basestate
         knockbackForce = new Vector3(0, knockbackForce.y -= motor.gravity * Time.deltaTime,
             knockbackForce.z += 2.0f * Time.deltaTime);
 
-        m.x = motor.SnapToLane();
-        m.y = -1.0f;
-        m.z = motor.baseRunspeed;
-        m.z = motor.baseRunspeed;
-        return m;
+        if(knockbackForce.z>0)
+        {
+            knockbackForce.z = 0;
+        //    GameManager.Instance.Changestate(GameManager.Instance.GetComponent<GameStateDearh>());
+        }
+
+     
+        return knockbackForce;
 
     }
 }
