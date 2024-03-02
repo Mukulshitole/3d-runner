df2c69db6eaecbc9b052f4a6b93ed61453cefe48
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-03-02 13:16:18 +0530

    just swipe up for a respawn

--- Assets/scripts/Playermotor/state/RespawnState.cs
+++ Assets/scripts/Playermotor/state/RespawnState.cs
@@ -7,6 +7,8 @@ public class RespawnState : Basestate
     [SerializeField] private float verticaldistance = 25.0f;
     public override void Construct()
     {
+        motor.controller.enabled = false;
         motor.transform.position = new Vector3(0, verticaldistance, motor.transform.position.z);
+        motor.controller.enabled = true;
     }
 }
