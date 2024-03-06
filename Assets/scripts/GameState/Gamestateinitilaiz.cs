fab10d14b7a244986521ea3706ebc3ec37baa344
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-03-06 22:25:51 +0530

    7hr36min

--- Assets/scripts/GameState/Gamestateinitilaiz.cs
+++ Assets/scripts/GameState/Gamestateinitilaiz.cs
@@ -1,16 +1,27 @@
 ﻿
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Gamestateinitilaiz : Gamestate
 {
+
+    public GameObject menuUI;
+    [SerializeField] private TextMeshProUGUI HighScoreText;
+    [SerializeField] private TextMeshProUGUI FishCountText;
     public override void Construct()
     {
         GameManager.Instance.ChangeCamera(GameCamera.Init);
+        HighScoreText.text = "HighScore : " + "TBD";
+    }
+   public void OnPlayClick()
+   {
+        brain.Changestate(GetComponent<GamestateGame>());
+
     }
 
-    public override void UpdateState()
+    public void OnShopClick() 
     {
-        if (InputManager.Instance.Tap)
-            brain.Changestate(GetComponent<GamestateGame>());
+        brain.Changestate(GetComponent<GamestateGame>());
     }
 }
