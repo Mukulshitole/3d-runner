3b0db8d9ad512a640bb956ce14561648254d882f
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-03-10 01:06:20 +0530

    second ui

--- Assets/scripts/GameState/GamestateGame.cs
+++ Assets/scripts/GameState/GamestateGame.cs
@@ -1,18 +1,26 @@
-﻿
-public class GamestateGame: Gamestate
+﻿using UnityEngine;
+using TMPro;
+public class GamestateGame : Gamestate
 {
-  
-  
+    public GameObject gameui;
+    [SerializeField] private TextMeshProUGUI fishcount;
+    [SerializeField] private TextMeshProUGUI Scorecount;
     public override void Construct()
     {
         base.Construct();
         GameManager.Instance.motor.ResumePlayer();
         GameManager.Instance.ChangeCamera(GameCamera.Game);
+        gameui.SetActive(true);
     }
+    public override void Destruct()
+    {
+        gameui.SetActive(false);
+    }
+
 
     public override void UpdateState()
     {
         GameManager.Instance.worldgeneration.scanposition();
         GameManager.Instance.ScenceChunkGeneration.scanposition();
     }
-} 
+}
