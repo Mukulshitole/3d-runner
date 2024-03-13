81e608c159296c7042969562cf256112c116937e
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-03-13 14:21:01 +0530

    8hr 8 min Update deathstatedeath
    fix animations added death state

--- Assets/scripts/GameState/GameStateDeath.cs
+++ Assets/scripts/GameState/GameStateDeath.cs
@@ -1,20 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameStateDeath : Gamestate
 {
+    public GameObject deathUI;
+    private float deathtime;
+    [SerializeField] private TextMeshProUGUI  highscore;
+    [SerializeField] private TextMeshProUGUI Currentscore;
+    [SerializeField] private TextMeshProUGUI  Fishtotal;
+    [SerializeField] private TextMeshProUGUI  currentFish;
+
+    // completion circle fileds
+    [SerializeField] private Image completionCircle;
+    public float timetoDecision = 2.5f; // time to decide
+
     public override void Construct()
     {
+
         base.Construct();
         GameManager.Instance.motor.PausePlayer();
+
+        deathtime = Time.time;
+        deathUI.SetActive(true);
+        completionCircle.gameObject.SetActive(true);
+        highscore.text = "Highscore : TBD ";
+        Currentscore.text = "?12345";  // question mark is put infront to know its temproary data
+        Fishtotal.text = "Total :TBD ";
+        currentFish.text = "x20";
+
+    }
+    public override void Destruct()
+    {
+        deathUI.SetActive(false);
     }
     public override void UpdateState()
     {
-        if (InputManager.Instance.Swipedown)
-            ToMenu();
-        if (InputManager.Instance.Swipeup)
-            ResumeGame();
+        //if (InputManager.Instance.Swipedown)
+        //    ToMenu();
+        //if (InputManager.Instance.Swipeup)
+        //    ResumeGame();
+        float ratio = (Time.time - deathtime) / timetoDecision;
+        completionCircle.color = Color.Lerp(Color.green, Color.red, ratio);
+        completionCircle.fillAmount = 1 - ratio;
+
+        if (ratio > 1)
+        {
+            completionCircle.gameObject.SetActive(false);
+        }
 
     }
     public void ResumeGame()
