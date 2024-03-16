e8aec7b0f868111cb6390f6a3f3d007ab37b6258
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-03-16 08:52:55 +0530

    8hr45min optimization

---
+++ Assets/scripts/Gamestats.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System;
+
+public class Gamestats : MonoBehaviour
+{
+   public static Gamestats Instance { get { return instance; } }
+    private static Gamestats instance;
+    // score 
+    public float score;
+    public float highscore;
+    public float distanceModifer = 1.5f;
+    //Fish
+    public int totalFish;
+    public int fishCollectedThisSession;
+    public float pointPerFish;
+
+    //Internal cooldown
+    private float lastScoreUpdate;
+    private float ScoreUpdateDelta = 0.2f;
+    
+    // Action 
+    public Action<int> OnCollectFish;
+    public Action<float> OnscoreChange;
+
+    public void Update()
+    {
+        float s = GameManager.Instance.motor.transform.position.z * distanceModifer;
+        s += fishCollectedThisSession * pointPerFish;
+
+        if (s > score)
+        {
+             score = s;
+            OnscoreChange?.Invoke(score);  // where costly we have to run it 60fps we have to call canvas every 60 sec
+        }
+          
+
+    }
+
+    public void CollectFish() 
+    {
+        fishCollectedThisSession++;
+        OnCollectFish?.Invoke(fishCollectedThisSession);
+    }
+
+
+}
+ 
