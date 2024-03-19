191f7e1bc270accfb9e9867b8a4010fb5026f9ec
Mukulshitole <102034299+Mukulshitole@users.noreply.github.com>
2024-03-19 23:28:56 +0530

    update savemanager 9hr 5 min

--- Assets/scripts/Save/SaveManager.cs
+++ Assets/scripts/Save/SaveManager.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.IO;
 using UnityEngine;
 
 public class SaveManager : MonoBehaviour
@@ -7,5 +7,26 @@ public class SaveManager : MonoBehaviour
     private static SaveManager instance;
 
     //Fields
+    public Savestate save;
     private const string saveFilename = "data.ss"; // FUN PART you can save file name in any format .anything
+
+
+    private void Awake()
+    {
+        Load();
+        // Try and load the previous load state
+    } 
+
+    private void Load()
+    {
+        try
+        { 
+          FileStream file = new FileStream(saveFilename, FileMode.Open, FileAccess.Read);
+
+        }
+        catch
+        {
+            Debug.Log("save file not found let create a new one !"); //catch create safe files
+        }
+    }
 }
